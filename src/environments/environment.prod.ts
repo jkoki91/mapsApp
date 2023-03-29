f4cf90581dfeab33e3f76a108984dff7a646f72a
Jeff Worker <>
2023-03-29 21:35:19 +0200

    Full screen map component

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    mapboxToken: 'pk.eyJ1Ijoiamtva2k5MSIsImEiOiJjbGZ0dGE2YjgwNDh1M2VvYnRma3EwZXJiIn0.wM-Vkx4yv6-5XaA-9OCIrA'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    mapboxToken: 'pk.eyJ1Ijoiamtva2k5MSIsImEiOiJjbGZ0dGE2YjgwNDh1M2VvYnRma3EwZXJiIn0.wM-Vkx4yv6-5XaA-9OCIrA'
+};
+
